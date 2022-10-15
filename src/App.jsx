e6e357544a1dba152a4d165d5e8c72955660376b
Hussien Kamal <Hussien.Kamal@linkdev.com>
2022-10-15 14:01:16 +0200

    base

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Product from './product';
+
+class App extends Component {
+  state = {}
+  render() {
+    return <Product />;
+  }
+}
+
+export default App;
---
+++ src/product.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Product = (x) => {
+  return <h1>lol</h1>;
+}
+
+export default Product;
