82d84b0a4477afbe93899ec65754c0a1e699235a
Hussien Kamal <Hussien.Kamal@linkdev.com>
2022-10-16 10:38:24 +0200

    Component of adding items

--- src/product.jsx
+++ src/product.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const Product = (x) => {
-  return <h1>lol</h1>;
+const Product = (props) => {
+
+  //States
+  const [device, setDevice] = useState([
+    { id: 0, product: 'laptop' },
+    { id: 1, product: 'screen' },
+  ]);
+  const [inputVal, setInputVal] = useState('');
+
+  //Functions
+  const add = () => {
+    setDevice([...device, { id: device.length, product: inputVal }])
+  }
+  const deleteBtn = (id) => {
+    setDevice(device.filter((ele) => ele.id !== id));
+  }
+
+  return (
+    <>
+      <input type="text" onChange={(e) => setInputVal(e.target.value)} />
+      <h3>Your Text: <span style={{ color: "grey" }} >{inputVal}</span></h3>
+      <button onClick={add}>ADD</button>
+      {device.map(ele => {
+        return <h1 key={ele.id}>{ele.product} <span style={{ color: "red", cursor: "pointer" }} onClick={() => deleteBtn(ele.id)}>  X</span></h1>
+      })}
+
+    </>
+  );
 }
 
 export default Product;
